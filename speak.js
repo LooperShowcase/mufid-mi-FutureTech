a786f60a48e74fe562643e1f67ca9b9d2d632780
Looper Project Showcases <87170787+LooperShowcase@users.noreply.github.com>
2023-07-14 18:20:58 +0300

    Update speak.js

--- speak.js
+++ speak.js
@@ -23,7 +23,10 @@ async function GPT_talk(question) {
     messages: conversationhistory,
   };
   var url = "https://api.openai.com/v1/chat/completions";
-  var apiKey = "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";
+  var apikey1 = "sk-jewzlrJimdKCWoNC";
+  var apikey2 = "x6CUT3BlbkFJ7rh";
+  var apikey3 = "AWckdyi3eQRL4AA4w";
+  var apiKey = apikey1 + apikey2 + apikey3;
   try {
     const response = await fetch(url, {
       method: "POST",
